1d6c24dfc0f6eb3c50f492869a2d9ba5e27ea620
unknown <brandon.geeman.lee@gmail.com>
2017-09-26 22:57:26 -0700

    Timer is working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,12 +3,26 @@ $(document).ready(function() {
 $("#maincontent").hide();
 $("#scorecard").hide();
 
+
+//When startQuiz button is clicked it will replace the button with the form content and start the timer.
 $("#startQuiz").on("click", function(){
-  console.log("Im working");
-  //When startQuiz button is clicked it will replace the button with the form content.
   document.getElementById("quizButton").innerHTML= "";
   $("#maincontent").show();
+  var counter = 10 //Adjust this to increase or decrease time
+  setInterval(function(){
+    counter --;
+      if(counter >= 0){
+        document.getElementById("timer").innerHTML = counter;
+      }
+      if(counter === 0){
+        clearInterval(counter);
+      }
+    }, 1000);
+  
+  
+
 })
+
 $("#submit").on("click", function(){
   console.log("Im working");
   //When startQuiz button is clicked it will replace the button with the form content.
@@ -16,5 +30,6 @@ $("#submit").on("click", function(){
   $("#maincontent").hide();
   $("#scorecard").show();
 })
+
 
 });
